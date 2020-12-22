bc545e4d5609d2193b2cb92654dd6bcad7579ef3
Lucas Matos <lucaspmatos@outlook.com>
2020-12-22 01:21:59 -0300

    Animating the Game - Part III

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+  this.classList.add("flip");
+
+  if (hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+
+  hasFlippedCard = false;
+
+  checkForMath();
+}
+
+function checkForMath() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unflipCard();
 }
 
 cards.forEach((card) => {
-    card.addEventListener('click', flipCard);
-})
+  card.addEventListener("click", flipCard);
+});
