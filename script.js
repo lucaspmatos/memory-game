12321b5cae36ea1ad910000f257125b2c17984a1
Lucas Matos <lucaspmatos@outlook.com>
2020-12-22 01:45:00 -0300

    Final Adjustments - Part I

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let lockBoard = false;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return;
 
   this.classList.add("flip");
 
@@ -31,13 +32,24 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    resetBoard();
   }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
